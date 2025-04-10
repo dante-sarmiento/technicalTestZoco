424a7cdf93b560c6a880c5374370817980a461c6
dante-sarmiento <dante.sarmientof5@gmail.com>
2025-04-10 10:55:29 -0300

    inicializando proyecto

---
+++ src/routes/appRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from '../pages/Login'
+import Dashboard from '../pages/Dashboard'
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+    )
+}
+
+export default AppRouter
