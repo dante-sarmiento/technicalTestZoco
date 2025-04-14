3654be03912cbf860f6d66d153866a0b42abaeb4
dante-sarmiento <dante.sarmientof5@gmail.com>
2025-04-14 17:55:34 -0300

    ajustes en obtencion de almacenamiento y obtencion de datos en contexto

--- src/api/users.js
+++ src/api/users.js
@@ -8,20 +8,3 @@ export const getUsers = async () => {
   }
   return USERS
 }
-
-export const login = (email, password) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const user = USERS.find(
-        (u) => u.email === email && u.password === password
-      )
-
-      if (user) {
-        const token = btoa(`${user.email}:${user.role}`)
-        resolve({ token, user })
-      } else {
-        reject(new Error('Credenciales inválidas'))
-      }
-    }, 1000)
-  })
-}
